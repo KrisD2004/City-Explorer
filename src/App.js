02de9da4ffbbe0d7d3f18ddf52cdd26b63b48b12
Kristian Dotson <KDotson387@gmail.com>
2023-05-03 08:52:49 -0500

    testtrial

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import ExploreLocation from './Exploring';
 import { useState } from 'react';
+// import axios library for making HTTP requests
 import axios from 'axios';
 
 function App() {
 
+// create a state variable called Location and a function to update it
   const [Location, setLocation]= useState('')
+  const [locationData, setLocationData] = useState(null);
 
+
+// create an asynchronous function to handle the city search event
   const CitySearch = async (event) => {
-    event.preventDefault()
-    const API = await axios.get( 'https://us1.locationiq.com/v1/search.php?key=pk.7dd36f4d0ae867a7a750660f3731be40&q=' + Location + '&format=json');
      
+    event.preventDefault()
+
+    
+    const API = await axios.get( `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KristianKey}&q=${Location}`);
+ 
+    //  console.log(API.data[0].display_name);
+
+      const { display_name, lat, lon } = API.data[0];
+      // setLocationData(display_name,lattitude,longitude)
+      setLocationData({ displayName: display_name, latitude: lat, longitude: lon });
+      console.log(API)
+
+
 
   }
+   // create a function to update the Location state variable when the user types in the input field
   const getLocation = (e) => {
     setLocation(e.target.value);
   }
+
+ // render the App component with the ExploreLocation component as a child
   return (
     <div className="App">
-       <ExploreLocation onCity = {CitySearch}  setLocate = {getLocation}/>
-        
+       <ExploreLocation onCity = {CitySearch}  setLocate = {getLocation}  setData = {setLocationData}/>
+        {/* <h1>{Location}</h1>
+        <p>{locationData}</p> */}
     </div>
   );
 }
--- src/Exploring.js
+++ src/Exploring.js
@@ -4,29 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 
 function ExploreLocation(props) {
-  
+    // render a form component with a form group containing a form control input field and a form text component to display the location
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label></Form.Label>
         <Form.Control onInput={props.setLocate}/>
         <Form.Text onInput={props.setLocate} className="text-muted">
-         {props.Location}
+         {/* {props.Location} {props.setData}   */}
+         {props.locationData && `${props.locationData.displayName} (${props.locationData.latitude}, ${props.locationData.longitude})`}
+
         </Form.Text>
       </Form.Group>
       <Button onClick={props.onCity} variant="primary" type="submit">
         Explore!
       </Button>
     </Form>
-    // <Form>
-    //   <FormGroup>
-    //     <FormLabel></FormLabel>
-    //     <FormText>
-    //         {props.Location}
-    //       </FormText>
-    //   </FormGroup>
-    //   <Button  onClick={props.onCity} variant="primary" type="submit"> Explore!</Button>
-    // </Form>
+    
   );
 }
 
