d1b130119c191a4d55cdcca59c11895123f9bb3c
Kristian Dotson <KDotson387@gmail.com>
2023-05-03 11:20:46 -0500

    added card

--- src/Exploring.js
+++ src/Exploring.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { FormGroup, Form, Button, FormLabel, FormText } from "react-bootstrap";
+import { FormGroup, Form, Button, FormLabel, FormText, Card } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 
 function ExploreLocation(props) {
-    // render a form component with a form group containing a form control input field and a form text component to display the location
+  // render a form component with a form group containing a form control input field and a form text component to display the location
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label></Form.Label>
-        <Form.Control onInput={props.setLocate}/>
-        <Form.Text onInput={props.setLocate} className="text-muted">
-         {/* {props.Location} {props.setData}   */}
-         {/* {props.locationData && `${props.locationData.displayName} (${props.locationData.latitude}, ${props.locationData.longitude})`} */}
+    <Card>
+      
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label></Form.Label>
+            <Form.Control onInput={props.setLocate} />
+            <Form.Text onInput={props.setLocate} className="text-muted">
+              {/* {props.Location} {props.setData}   */}
+              {/* {props.locationData && `${props.locationData.displayName} (${props.locationData.latitude}, ${props.locationData.longitude})`} */}
 
-        </Form.Text>
-      </Form.Group>
-      <Button onClick={props.onCity} variant="primary" type="submit">
-        Explore!
-      </Button>
-    </Form>
+            </Form.Text>
+          </Form.Group>
+          <Button onClick={props.onCity} variant="primary" type="submit">
+            Explore!
+          </Button>
+        </Form>
+    </Card>
+    
     
   );
 }
