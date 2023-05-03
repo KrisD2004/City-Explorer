3e20ed1d1a3288f4248a1fa8f66aa0f5209de99f
Kristian Dotson <KDotson387@gmail.com>
2023-05-03 09:34:33 -0500

    added location data

--- src/App.js
+++ src/App.js
@@ -7,43 +7,46 @@ import axios from 'axios';
 
 function App() {
 
-// create a state variable called Location and a function to update it
-  const [Location, setLocation]= useState('')
+  // create a state variable called Location and a function to update it
+  const [Location, setLocation] = useState('')
   const [locationData, setLocationData] = useState(null);
 
 
-// create an asynchronous function to handle the city search event
+  // create an asynchronous function to handle the city search event
   const CitySearch = async (event) => {
-     
+
     event.preventDefault()
 
-    
-    const API = await axios.get( `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_KristianKey}&q=${Location}&format=json`);
- 
+
+    const API = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_KristianKey}&q=${Location}&format=json`);
+
     //  console.log(API.data[0].display_name);
 
-      const { display_name, lat, lon } = API.data[0];
-      // setLocationData(display_name,lattitude,longitude)
-      setLocationData({ displayName: display_name, latitude: lat, longitude: lon });
-      console.log(API)
+    const { display_name, lat, lon } = API.data[0];
+    // setLocationData(display_name,lattitude,longitude)
+    setLocationData({ displayName: display_name, latitude: lat, longitude: lon });
+    console.log(API)
 
 
 
   }
-   // create a function to update the Location state variable when the user types in the input field
+  // create a function to update the Location state variable when the user types in the input field
   const getLocation = (e) => {
     setLocation(e.target.value);
   }
 
- // render the App component with the ExploreLocation component as a child
+  // render the App component with the ExploreLocation component as a child
   return (
     <div className="App">
-       <ExploreLocation onCity = {CitySearch}  setLocate = {getLocation}  setData = {setLocationData}/>
-        {/* <h1>{Location}</h1>
-        <p>{locationData}</p> */}
+      <ExploreLocation onCity={CitySearch} setLocate={getLocation} setData={setLocationData} />
+      <h1>{Location.display_name}</h1>
+      <p>{locationData.displayName}</p>  
+      <p>{locationData.latitude}</p> 
+      <p>{locationData.longitude}</p>    
+  {/* {locationData && locationData.displayName ? locationData.latitude && locationData.longitude} */}
+      {/* <p>{locationData}</p> */}
     </div>
   );
 }
 
 export default App;
- 
